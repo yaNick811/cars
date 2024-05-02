da3fdb3704e965e8d09f915758fbec5deba497a4
yaNick811 <167251929+yaNick811@users.noreply.github.com>
2024-05-02 14:14:59 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+print(f'Я езжу на автомабиле марки {cars}')
+cars_count = 0
+for car in cars:
+    print(f'Я езжу на автомабиле марки {car}')
+    cars_count += 10
+print(f"Всего я езжу на {cars_count} автомобилях")
+
